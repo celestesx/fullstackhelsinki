65733bcbe87d440289f442e10b2183177349a765
Liane <sarahliane87@gmail.com>
2025-08-25 12:10:46 +1000

    start part d

--- part1/component_State_eventhandlers/eventhandlers/src/App.jsx
+++ part1/component_State_eventhandlers/eventhandlers/src/App.jsx
@@ -1,27 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
-const Hello = (props) => {
-  return (
-    <div>
-      <p>
-        Hello {props.name}, you are {props.age} years old
-      </p>
-    </div>
-  )
-}
+// const Display = (props) => {
+//   return (
+//     <div>{props.counter}</div>
+//   )
+// }
+//We destructure the above
+const Display = ({counter}) => <div>{counter}</div>
+
+// const Button = (props) => {
+//   return (
+//     <button onClick={props.onClick}>
+//       {props.text}
+//     </button>
+//   )
+// }
+//We destructure the above
+const Button = ({onClick, text}) => <button onClick = {onClick}>{text}</button>
 
 const App = () => {
-  const name = 'Peter'
-  const age = 10
+  const [counter, setCounter] = useState(0)
+  console.log('rendering with counter value', counter)
+
+  const increaseByOne = () => {
+    console.log('increasing, value before', counter)
+    setCounter(counter+1)
+  }
+
+  const decreaseByOne = () => {
+    console.log('decreasing, value before', counter)
+    setCounter(counter -1)
+  }
+
+  const setToZero = () => {
+    console.log('resetting to zero, value before', counter)
+    setCounter(0)
+  }
 
   return (
     <div>
-      <h1>Greetings</h1>
-      <Hello name = "Maya" age = {26 + 10} />
-      
+      <Display counter = {counter}/>
+      <Button onClick = {increaseByOne} text ='plus'/>
+      <Button onClick = {setToZero} text ='zero'/>
+      <Button onClick = {decreaseByOne} text ='minus'/>
     </div>
   )
 }
